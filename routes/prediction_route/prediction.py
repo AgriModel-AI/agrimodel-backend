2687b948ae28c437b62e16226e5a99f3fbd5cb35
Mugisha Abdoul Latif <85370865+MugAbdoul@users.noreply.github.com>
2025-03-22 15:18:04 +0200

    Update requirements

--- routes/prediction_route/prediction.py
+++ routes/prediction_route/prediction.py
@@ -6,6 +6,8 @@ from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models import Crop, DiagnosisResult, Disease, Notification, UserDetails, db
 from .tflite_inference import TFLitePlantDiseaseInferencePipeline
+from sqlalchemy import func
+import cloudinary.uploader
 
 # Initialize TFLite model pipeline
 pipeline = TFLitePlantDiseaseInferencePipeline(
@@ -24,70 +26,6 @@ def allowed_file(filename):
     """Check if the uploaded file has a valid extension."""
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# class PredictionResource(Resource):
-#     @jwt_required()
-#     def post(self):
-#         """Handles image upload and plant disease prediction."""
-#         user_identity = get_jwt_identity()
-#         userId = int(user_identity["userId"])
-
-#         # Validate image file
-#         image = request.files.get('image')
-#         if not image or not allowed_file(image.filename):
-#             return {"message": "A valid image file (png, jpg, jpeg, gif) is required."}, 400
-
-#         all_results = []
-#         start_time = time.time()
-
-#         try:
-#             # Save the file temporarily
-#             file_path = os.path.join(TEMP_DIR, image.filename)
-#             image.save(file_path)
-
-#             # Perform prediction
-#             result = pipeline.test_time_augmentation(file_path)
-
-#             # Add file info to result
-#             result['filename'] = image.filename
-#             all_results.append(result)
-
-#             # Remove temporary file
-#             os.remove(file_path)
-        
-#         except Exception as e:
-#             all_results.append({
-#                 'filename': image.filename,
-#                 'error': f'Prediction error: {str(e)}'
-#             })
-
-#         # Calculate prediction time
-#         prediction_time = time.time() - start_time
-
-#         # Return response
-#         # if len(all_results) == 1:
-#         #     result = all_results[0]
-#         #     result['prediction_time'] = f"{prediction_time:.3f} seconds"
-#         #     return jsonify(result)
-
-#         # response = {
-#         #     'results': all_results,
-#         #     'prediction_time': f"{prediction_time:.3f} seconds",
-#         #     'count': len(all_results)
-#         # }
-        
-#         response = {
-#             "detected": all_results[0]["plant_type"] in ['banana', 'coffee'],
-#             "disease_status": all_results[0]["disease_status"],
-#             "plant_type": all_results[0]["plant_type"],
-#             "prediction_time" : f"{prediction_time:.3f} seconds",
-#         }
-        
-
-#         return jsonify(response)
-
-
-from sqlalchemy import func
-import cloudinary.uploader
 
 class PredictionResource(Resource):
     @jwt_required()
@@ -98,6 +36,9 @@ class PredictionResource(Resource):
 
         # Validate image file
         image = request.files.get('image')
+        if not image:
+            return {"message": "Please send Image"}, 400
+        
         if not image or not allowed_file(image.filename):
             return {"message": "A valid image file (png, jpg, jpeg, gif) is required."}, 400
 
@@ -175,6 +116,8 @@ class PredictionResource(Resource):
             
             # Remove temporary file
             os.remove(file_path)
+            
+            print(response)
             
             return jsonify(response)
         
